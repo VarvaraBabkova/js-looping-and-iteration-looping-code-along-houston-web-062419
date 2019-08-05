029cb254f98468ec4a2a04d0fce63ac7a9d2a598
Varvara Babkova <varvara.munday@gmail.com>
2019-08-05 20:13:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function writeCards (names, cause)
   let cards = [];
   for (let i = 0; i < names.length - 1; i++)
   {
-    cards.push("Thank you, ${names[i]}, for the wonderful ${cause} gift.");
+    cards.push('Thank you, ${names[i]}, for the wonderful ${cause} gift.');
   }
   return cards;
 }
